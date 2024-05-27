ab07107dab27af75c32d57e6a6ca2a14ea12abad
Manish Kumar <manish.kumar@basf.com>
2024-05-27 14:50:21 +0200

    conf update

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import pathlib
 import sys
-import os
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
@@ -46,15 +46,83 @@ extensions = [
     "sphinx_autodoc_typehints",
 ]
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-language = 'english'
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.md']
+# source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
+
+# This is also used if you do content translation via gettext catalogs.
+# Usually you set "language" from the command line for these cases.
+language = "en"
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = ["_build", "_templates"]
+
+# The reST default role (single back ticks `dict`) cross links to any code
+# object (including Python, but others as well).
+default_role = "literal"
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# If true, sectionauthor and moduleauthor directives will be shown in the
+# output. They are ignored by default.
+show_authors = False
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# 'sphinx_book_theme'  # 'furo', 'press', 'pydata_sphinx_theme'
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
+# html_css_files = ['custom.css']
+# html_js_files = ['custom.js']
 
-# in this case path_to_root_directory is fancy_calcy
+html_theme_options = {
+    'globaltoc_collapse': False,
+}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "images/logo/mamba_tabular.png"
+
+# Override the Sphinx default title that appends `documentation`
+html_title = f'{project}'
+# Format of the last updated section in the footer
+html_last_updated_fmt = '%Y-%m-%d'
+
+# -- Options for autodoc ------------------------------------------------------
+
+autodoc_default_options = {
+    "members": True,
+    "inherited-members": True,
+    "exclude-members": "set_output",
+}
+
+# generate autosummary even if no references
+autosummary_generate = True
+
+# -- Options for numpydoc -----------------------------------------------------
+
+# this is needed for some reason...
+# see https://github.com/numpy/numpydoc/issues/69
+
+numpydoc_show_class_members = False
