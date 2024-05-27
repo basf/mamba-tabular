8b30d03394373d745af647f01bce277509253461
Manish Kumar <manish.kumar@basf.com>
2024-05-27 15:12:34 +0200

    typo for module

--- mambular/__init__.py
+++ mambular/__init__.py
@@ -1,4 +1,5 @@
 from . import base_models, models
 from .__version__ import __version__
+from .utils import Preprocessor
 
-__all__ = ['base_models', 'models', '__version__']
+__all__ = ['base_models', 'models', 'Preprocessor', '__version__']
