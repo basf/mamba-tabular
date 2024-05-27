787c10eb92b2717bae05f0248d70290a39a882c4
Manish Kumar <manish.kumar@basf.com>
2024-05-27 18:39:12 +0200

    import issue fixed

--- mambular/models/sklearn_embedding_classifier.py
+++ mambular/models/sklearn_embedding_classifier.py
@@ -2,7 +2,7 @@ import lightning as pl
 import numpy as np
 import pandas as pd
 import torch
-from lightning.callbacks import EarlyStopping, ModelCheckpoint
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.base import BaseEstimator
 from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
