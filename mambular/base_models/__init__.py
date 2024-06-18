83dbe2c79ace310790f02ba0a49b24f7fbdc6e34
AnFreTh <antonthielmann@t-online.de>
2024-06-18 16:43:49 +0000

    include ft-transformer into basemodels

--- mambular/base_models/__init__.py
+++ mambular/base_models/__init__.py
@@ -1,9 +1,11 @@
 from .embedding_classifier import BaseEmbeddingMambularClassifier
 from .embedding_regressor import BaseEmbeddingMambularRegressor
 from .lightning_wrapper import TaskModel
+from .mambular_base import Mambular
 
 __all__ = [
     "BaseEmbeddingMambularRegressor",
     "BaseEmbeddingMambularClassifier",
     "TaskModel",
+    "Mambular",
 ]
