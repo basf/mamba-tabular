43aaf98a2cb8a85434130ab83f70bd12a7a29a58
AnFreTh <antonthielmann@t-online.de>
2024-06-28 12:18:59 +0000

    adjusted doc strings for config files

--- mambular/configs/mlp_config.py
+++ mambular/configs/mlp_config.py
@@ -4,6 +4,39 @@ import torch.nn as nn
 
 @dataclass
 class DefaultMLPConfig:
+    """
+    Configuration class for the default Multi-Layer Perceptron (MLP) model with predefined hyperparameters.
+
+    Parameters
+    ----------
+    lr : float, default=1e-04
+        Learning rate for the optimizer.
+    lr_patience : int, default=10
+        Number of epochs with no improvement after which learning rate will be reduced.
+    weight_decay : float, default=1e-06
+        Weight decay (L2 penalty) for the optimizer.
+    lr_factor : float, default=0.1
+        Factor by which the learning rate will be reduced.
+    layer_sizes : list, default=(128, 128, 32)
+        Sizes of the layers in the MLP.
+    activation : callable, default=nn.SELU()
+        Activation function for the MLP layers.
+    skip_layers : bool, default=False
+        Whether to skip layers in the MLP.
+    dropout : float, default=0.5
+        Dropout rate for regularization.
+    norm : str, default=None
+        Normalization method to be used, if any.
+    use_glu : bool, default=False
+        Whether to use Gated Linear Units (GLU) in the MLP.
+    skip_connections : bool, default=False
+        Whether to use skip connections in the MLP.
+    batch_norm : bool, default=False
+        Whether to use batch normalization in the MLP layers.
+    layer_norm : bool, default=False
+        Whether to use layer normalization in the MLP layers.
+    """
+
     lr: float = 1e-04
     lr_patience: int = 10
     weight_decay: float = 1e-06
