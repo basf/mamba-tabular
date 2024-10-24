9a04905b36d16c23bb26f61b7175e9c85503abf9
AFThielmann <antonthielmann@t-online.de>
2024-10-24 21:21:07 +0200

    include configs in package

---
+++ mambular/configs/__init__.py
@@ -0,0 +1,20 @@
+from .mambular_config import DefaultMambularConfig
+from .fttransformer_config import DefaultFTTransformerConfig
+from .resnet_config import DefaultResNetConfig
+from .mlp_config import DefaultMLPConfig
+from .tabtransformer_config import DefaultTabTransformerConfig
+from .mambatab_config import DefaultMambaTabConfig
+from .tabularnn_config import DefaultTabulaRNNConfig
+from .mambattention_config import DefaultMambAttentionConfig
+
+
+__all__ = [
+    "DefaultMambularConfig",
+    "DefaultFTTransformerConfig",
+    "DefaultResNetConfig",
+    "DefaultMLPConfig",
+    "DefaultTabTransformerConfig",
+    "DefaultMambaTabConfig",
+    "DefaultTabulaRNNConfig",
+    "DefaultMambAttentionConfig",
+]
