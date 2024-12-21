dceeb93ca6a8b4e87758662d980bd9863fa9ea9c
Manish Kumar <manish.kumar@basf.com>
2024-12-21 20:03:10 +0100

    refactor examples

--- examples/example_distributional.py
+++ examples/example_distributional.py
@@ -28,9 +28,7 @@ X = data.drop(columns=["target"])
 y = np.array(data["target"])
 
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 # Instantiate the regressor
